eb333ee33d356a95d85b687d744b257ae0a256b8
Joung MyeongHee <jmhee3411@gmail.com>
2025-01-03 14:38:16 +0900

    t8 blue

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class App {
     private final Scanner sc;
-
+    private int lastId;
     public App(Scanner sc){
         this.sc = sc;
+        this.lastId = 0;
     }
     public void run(){
 
@@ -22,7 +23,8 @@ public class App {
             } else if (cmd.equals("등록")) {
                 System.out.println("명언 : ");
                 System.out.println("작가 : ");
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastId));
+
             }
         }
     }
